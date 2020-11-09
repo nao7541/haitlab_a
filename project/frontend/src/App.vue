1600a7599e28bae73518c6f877e03e32c633eeae
Jun Nishimura <n.junjun0303@gmail.com>
2020-11-10 00:13:37 +0900

    Vue Version Change from Vue2 to Vue3

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -1,22 +1,30 @@
 <template>
-    <div id="app">
-        <h1>Haitlab A</h1>
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-export default {
-    name: 'App',
-
-}
-</script>
-
-<style> 
-* {
-    box-sizing: border-box;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-    margin: 0;
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
