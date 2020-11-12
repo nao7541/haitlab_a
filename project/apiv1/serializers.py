3accb1be33986a6cbc10b8b6d7f095267ebb4a0c
nao73 <kanskce7541@gmail.com>
2020-11-13 01:23:56 +0900

    Tagモデルの調整
    skillに関連する項目の削除とTagとCustomuserの紐づけ

--- project/apiv1/serializers.py
+++ project/apiv1/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
-from user.models import CustomUser, SKILL_CHOICES
+from user.models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
-    # username = None
-    skills = serializers.MultipleChoiceField(choices=SKILL_CHOICES, allow_blank=True)
-
     class Meta:
-        fields = ('email', 'prof_img', 'intro', 'univ_name', 'major', 'skills','contact')
+        fields = ('email', 'prof_img', 'intro', 'univ_name', 'major', 'contact')
         read_only_fields = ( 'user_id', 'username',)
         model = CustomUser
