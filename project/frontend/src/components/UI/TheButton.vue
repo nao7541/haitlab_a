f40c6ba30dc7af9e7c5c2f57df6c20d52c9cb1a9
Jun Nishimura <n.junjun0303@gmail.com>
2020-11-12 19:26:09 +0900

    Add new Signup Page and Login Page

---
+++ project/frontend/src/components/UI/TheButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button>{{ text }}</button>
+</template>
+
+<script>
+export default {
+    props: ['text'],
+}
+</script>
+
+<style scoped>
+button {
+    font-size: 16px;
+    font-weight: bold;
+    color: #000;
+    border-radius: 8px;
+    border: 1px solid #ffffff;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.26);
+    background-color: #ffffff;
+    padding: 1rem 3rem;
+    outline: none;
+}
+
+button:focus {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
