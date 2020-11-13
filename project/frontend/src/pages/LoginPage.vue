5f1efe259f119738705549c7351c07c8789f73c3
Jun Nishimura <n.junjun0303@gmail.com>
2020-11-13 23:41:44 +0900

    Update SignUp and Login pages

--- project/frontend/src/pages/LoginPage.vue
+++ project/frontend/src/pages/LoginPage.vue
@@ -1,37 +1,25 @@
 <template>
-    <div id="login-page">
+    <div id="signup-page">
         <BaseCard class="base-card">
-            <img class="user-logo" src="../assets/user.png" alt="user">
-            <h1 class="title">ログイン</h1>
-            <form>
-                <div class="registration">
-                    <div class="input-registration">
-                        <div class="form-control">
-                            <input type="text" id="username" name="username" placeholder="username">
-                        </div>
-                        <div class="form-control">
-                            <input type="email" id="email" name="email" placeholder="mail address">
-                        </div>
-                        <div class="form-control">
-                            <input type="password" id="password" name="password" placeholder="password">
-                        </div>
-                        <TheButton @click="acceptUser" text="ログイン"></TheButton>
-                    </div>
-                    <div class="ref-registration">
-                        <div class="form-control">
-                            <TheButton class="facebook" text="Facebookで作成"></TheButton>
-                        </div>
-                        <div class="form-control">
-                            <TheButton class="twitter" text="Twitterで作成"></TheButton>
-                        </div>
-                        <div class="form-control">
-                            <TheButton class="google" text="Googleで作成"></TheButton>
-                        </div>
-                        <div class="form-control">
-                            <TheButton class="apple" text="Appleで作成"></TheButton>
-                        </div>
-                    </div>
+            <form @submit.prevent="acceptUser">
+                <h1>Log In</h1>
+                <div class="form-control">
+                    <p>Username</p>
+                    <input type="text" name="username">
                 </div>
+                <div class="form-control">
+                    <p>Password</p>
+                    <input type="password" name="password">
+                </div>
+                <TheButton text="Submit" class="submit"/>
+                <div class="form-control forget">
+                    <a href="#">forget password?</a>
+                </div>
+                <div class="or">
+                    <p>OR</p>
+                </div>
+                <TheButton text="LogIn with Facebook" class="facebook"/>
+                <TheButton text="LogIn with Twitter" class="twitter"/>
             </form>
         </BaseCard>
     </div>
@@ -46,7 +34,7 @@ export default {
     },
     methods: {
         acceptUser() {
-            this.$router.push('/home');
+            this.$router.push('/ideas');
         }
     }
 }
@@ -55,100 +43,86 @@ export default {
 <style scoped>
 
 .base-card {
-    margin-top: 5rem;
-    padding: 0 2rem;
-}
-
-.user-logo {
-    background-color: #fff;
-    border-radius: 64px;
-    width: 128px;
-    height: 128px;
-    margin-top: -64px;
-}
-
-.title {
-    border-bottom: 1px solid #000000;
-    margin-bottom: 1rem;
-    padding: 1.2rem 0;
+    max-width: 30rem;
 }
 
 form {
-    grid-area: form;
+    padding: .25rem 2rem;
+}
+
+form h1 {
+    font-size: 28px;
+    padding: 1rem 0;
 }
 
 .form-control {
     margin-bottom: 1rem;
 }
 
-.registration {
-    display: grid;
-    grid-template-columns: 1fr 2.25fr;
-    grid-template-areas: 
-            "input ref";
+.form-control p {
+    text-align: left;
+    margin: 0;
+    font-weight: bold;
 }
 
-.input-registration {
-    grid-area: input;
-    margin: 2rem 0;
-}
-
-.input-registration button {
-    width: 100%;
-}
-
-input {
+.form-control input {
     font-size: 16px;
     width: 100%;
-    padding: 1rem 0.5rem;
-    border-radius: 4px;
-    border: 1px solid #000;
-    box-shadow: 0 0.5px 1px rgba(0, 0, 0, 0.25);
+    padding: .5rem;
+    border: none;
+    border-bottom: 1px solid #aaaaaa;
+    outline: none;
 }
 
-input:focus {
-    transition: all .5s ease;
-    border: 1px solid #df9203;
-    background-color: #ffe1a9;
-    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.25);
+.form-control input:focus {
+    border-bottom: 2px solid #ffb01e;
 }
 
-.input-registration button {
-    border: 1px solid #ffeece;
+form button {
+    margin-bottom: 1rem;
+}
+
+.submit {
+    border-color: #ffeece;
     background-color: #ffeece;
 }
 
-.input-registration button:hover {
+.submit:hover {
+    transition: all .25s ease-in;
     border: 1px solid #ffe0a7;
     background-color: #ffe0a7;
 }
 
-.input-registration button:focus {
-    border: 1px solid #ffca67;
-    background-color: #ffca67;
+.form-control a {
+    text-decoration: none;
+    color: #a7a7a7;
+    line-height: 2rem;
 }
 
-.ref-registration {
-    grid-area: ref;
-    border-left: 1px solid #9e9e9e75;
-    margin-left: 1rem;
-    margin-bottom: 2rem;
+.form-control a:hover {
+    color:  #ffbb3c;
 }
 
-.ref-registration button {
-    width: 80%;
+.forget {
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #a7a7a7;
+}
+
+.or {
+    text-align: center;
+    margin-top: -30px;
 }
 
 .facebook {
+    color: #eeeeee;
+    border-color: 1px solid #3A559F;
     background-color: #3A559F;
 }
 
 .twitter {
+    color: #eeeeee;
+    border-color: 1px solid #50ABF1;
     background-color: #50ABF1;
-}
-
-.google .apple {
-    background-color: #ffffff;
 }
 
 </style>
