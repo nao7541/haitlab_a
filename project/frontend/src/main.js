f1ad469c20a3ba803f106d00521f3e66ce5f2946
Jun Nishimura <n.junjun0303@gmail.com>
2020-11-25 13:44:48 +0900

    add fontawesome

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -3,12 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//---------- Font Awesome ----------//
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebookSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faFacebookSquare, faTwitterSquare);
+
+//---------- Base Components ----------//
 import BaseCard from '@/components/UI/BaseCard.vue';
 import BaseButton from '@/components/UI/BaseButton.vue';
 import BaseCheckBox from '@/components/UI/BaseCheckBox.vue';
 import BaseTag from '@/components/UI/BaseTag.vue';
 
 const app = createApp(App);
+// add font awesome
+app.config.productionTip = false;
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+// add base components
 app.component('BaseCard', BaseCard);
 app.component('BaseButton', BaseButton);
 app.component('BaseCheckBox', BaseCheckBox);
