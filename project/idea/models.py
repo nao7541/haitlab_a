73630a32703a24d8e7c8c7ec540d41b7aef2f883
Jun Nishimura <n.junjun0303@gmail.com>
2020-12-15 19:11:50 +0900

    remove not null constraint at Idea model for offer, deadline, uniqueness, novelty, possibility

--- project/idea/models.py
+++ project/idea/models.py
@@ -15,9 +15,11 @@ class PostIdea(models.Model):
    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="アイデア投稿ユーザ")
    # タイトル
    title = models.CharField(default="Idea Title", max_length=100, verbose_name='タイトル')
-   # 投稿 文字列
+   # 概要
    overview = models.TextField(max_length=500, blank=True, null=True, verbose_name="概要")
+   # 背景
    background = models.TextField(max_length=500, blank=True, null=True, verbose_name="背景")
+   # 熱意、オモイ
    passion = models.TextField(max_length=500, blank=True, null=True, verbose_name="思い")
    # 投稿 画像
    idea_image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name="投稿画像") # upload_to settings - MEDIA_ROOT以下のファイル保存先
@@ -26,17 +28,17 @@ class PostIdea(models.Model):
    # 状態
    state = models.CharField(max_length=100, verbose_name='状態')
    # ターゲット
-   target = models.CharField(max_length=100, verbose_name='ターゲット')
+   target = models.CharField(max_length=100, blank=True, null=True, verbose_name='ターゲット')
    # 人材募集
-   offer = models.CharField(max_length=100, verbose_name='人材募集')
+   offer = models.CharField(max_length=100, blank=True, null=True, verbose_name='人材募集')
    # 期日
-   deadline = models.CharField(max_length=30, verbose_name='期日')
+   deadline = models.CharField(max_length=30, blank=True, null=True, verbose_name='期日')
    # ユニークさ
-   uniqueness = models.IntegerField(default=0, verbose_name='ユニークさ')
+   uniqueness = models.IntegerField(default=0, blank=True, null=True, verbose_name='ユニークさ')
    # 新規性
-   novelty = models.IntegerField(default=0, verbose_name='新規性')
+   novelty = models.IntegerField(default=0, blank=True, null=True, verbose_name='新規性')
    # 実現可能性
-   possibility = models.IntegerField(default=0, verbose_name='実現可能性')
+   possibility = models.IntegerField(default=0, blank=True, null=True, verbose_name='実現可能性')
    # イベント外部キー
    event_id = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True, verbose_name='イベント')
 
