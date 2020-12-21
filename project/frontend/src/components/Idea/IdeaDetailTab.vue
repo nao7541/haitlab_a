1fe6f6b7241f602f672c5607307fd6de2fae1718
Jun Nishimura <n.junjun0303@gmail.com>
2020-12-22 00:19:53 +0900

    Update IdeaDetail

---
+++ project/frontend/src/components/Idea/IdeaDetailTab.vue
@@ -0,0 +1,56 @@
+<template>
+    <ul>
+        <li><router-link :to="overviewLink">概要</router-link></li>
+        <li><router-link :to="detailInfoLink">詳細</router-link></li>
+        <li><router-link :to="requirementsLink">募集</router-link></li>
+        <li><router-link :to="feedbackLink">フィードバック</router-link></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    computed: {
+        overviewLink() {
+            return { name: 'overview' };
+        },
+        detailInfoLink() {
+            return { name: 'detailInfo' };
+        },
+        requirementsLink() {
+            return { name: 'requirements' };
+        },
+        feedbackLink() {
+            return { name: 'feedback' };
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid #ddd;
+}
+
+a {
+    padding: 0 1rem;
+    line-height: 4rem;
+    display: block;
+    font-size: 18px;
+    font-weight: bold;
+    text-decoration: none;
+    color: #000;
+}
+
+li:hover {
+    background-color: #ffefd1;
+}
+
+.router-link-active {
+    border-bottom: 2px solid #ffbb3c;
+}
+</style>
