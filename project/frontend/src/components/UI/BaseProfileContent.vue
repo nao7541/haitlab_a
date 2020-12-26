7afd700fa2e03eeb627ede897a5d69c7652c451a
Jun Nishimura <n.junjun0303@gmail.com>
2020-12-26 09:51:32 +0900

    ADD scroll for item list

--- project/frontend/src/components/UI/BaseProfileContent.vue
+++ project/frontend/src/components/UI/BaseProfileContent.vue
@@ -4,7 +4,9 @@
             <slot name="contentHeaderLinks"></slot>
         </div>
         <div class="content-body">
-            <slot name="contentBody"></slot>
+            <div class="item-list">
+                <slot name="contentBody"></slot>
+            </div>
         </div>
     </div>
 </template>
@@ -18,5 +20,24 @@
 .content-body {
     background-color: #fff;
     height: 50rem;
+}
+
+.item-list {
+    height: 100%;
+    overflow: scroll;
+}
+
+.item-list::-webkit-scrollbar {
+    background-color: #fff;
+    width: 10px;
+}
+
+.item-list::-webkit-scrollbar-thumb {
+    background-color: #eaeaea;
+    border-radius: 8px;
+}
+
+.item-list::-webkit-scrollbar-button {
+    display: none;
 }
 </style>
